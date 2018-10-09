41a2e3b5bc0f87e788060d4aac3955780e90ce3b
Momo_Ibn_Fatou <43034190+MomoNdour@users.noreply.github.com>
2018-10-09 12:32:39 +0000

    troisieme commit

--- index.php
+++ index.php
@@ -1,14 +1,61 @@
+<?php
+// Affichage des dossiers et des fichiers
+function lister_dossier(){
+$racine="../" ;//chemin du dossier
+if(isset($_GET['dossier'])){
+    $dossier = ($_GET['dossier']);
+    $racine=$racine.$dossier; 
+}
+
+$dirs = scandir($racine);  
+
+foreach ($dirs as $dir) {
+    # code...
+    if (is_dir($racine.$dir)) {
+        # code...
+        if ($dir == "..") {
+            # code...
+        } else {
+            # code...
+            if (isset($_GET['dossier'])) {
+                # code...
+                ?>
+                <a style="color:black;" href="index.php?dossier=<?php echo $dossier.$dir; ?>">
+                <img src='images/dossier.png' alt=''width='30'><?php echo $dir; ?>
+                </a>
+                <br>
+                <?php
+            } else {
+                # code...
+                ?>
+                <a style="color:black;" href="index.php?dossier=<?php echo $dir; ?>">
+                <img src='images/dossier.png' alt='' width='30'><?php echo $dir; ?>
+                </a><br>
+                <?php
+            }
+        }
+    } else {
+        # code...
+        echo "
+                <a>
+                <img src='images/fichier.png' alt=''width='30'></a>".$dir."
+                </a><br>
+            ";
+    }
+}
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Explorateur de fichier</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/moncss.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
 </head>
-
 <body>
     <!-- Container fluid -->
 <div class="container-fluid">
@@ -29,23 +76,30 @@
                     <div class="section-explorateur">
                         <!-- Dossier -->
                         <div class="col-md-9 dossier">
-                            <?php include "Liste_dir.php"; ?>
+                            <?php lister_dossier() ?>
                         </div><!-- Fin dossier -->
 
                         <!-- Les fonctionnalites -->
                         <div class="col-md-3 fonctionnalite">
-                            <form action="traitement.php?racine=<?= "../" ?>" method="POST">
+                            <form action="traitement.php?racine=<?= "../"?>" method="POST">
                                 <br><div>
-                                <input type='text' name='dossier'/>
-                                <button data-toggle="tooltip" title="Ajouter un dossier">
+                                <input type='text' style="color:black;" name='dossier'/>
+                                <button type="submit" data-toggle="tooltip" title="Ajouter un dossier">
                                     <img src="images/nouveau_dossier.png" alt="" width="20px" height="20px">
                                 </button>
                                 </div><br>
 
                                 <div>
-                                <input type='text' name='dossier1'/>
-                                <button data-toggle="tooltip" title="Supprimer un dossier">
-                                 <img src="images/supprimer.png" alt="" width="20px" height="20px">
+                                <input type='text' style="color:black;" name='dossier1'/>
+                                <button type="submit" data-toggle="tooltip" title="Supprimer un dossier">
+                                    <img src="images/supprimer.png" alt="" width="20px" height="20px">
+                                </button>
+                                </div><br>
+
+                                <div>
+                                <input type='text'style="color:black;" name='exname'/><input type='text' name='newname'/><br>
+                                <button type="submit" data-toggle="tooltip" title="Ajouter un fichier">
+                                    <img src="images/renommer.png" alt="" width="20px" height="20px">
                                 </button>
                                 </div><br>
 
@@ -56,9 +110,6 @@
             </div><!-- Fin row -->
     </div><!-- Fin contenu -->
 </div><!-- Fin container fluid -->
-
-
-
 
 
 <!-- fonction creation de dossier--->
@@ -76,11 +127,10 @@ if(!empty($_POST['dossier'])){
     creer_dossier($_POST['dossier']);
 }
 ?>
-
  <!-- fonction suppression de dossier--->
 <?php
 function supprimer($dossier) {
-     if (is_dir($dossier)) { // si le paramètre est un dossier
+     if(is_dir($dossier)) { // si le paramètre est un dossier
          $objects = scandir($dossier); // on scan le dossier pour récupérer ses objets
          foreach ($objects as $object) { // pour chaque objet
               if ($object != "." && $object != "..") { // si l'objet n'est pas . ou ..
@@ -89,7 +139,7 @@ function supprimer($dossier) {
                   }
          }
          reset($objects); // on remet à 0 les objets
-         rmdir($dossier); // on supprime le dossier
+         supprimer($dossier); // on supprime le dossier
         }
  }
 ?>
